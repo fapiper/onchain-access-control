5d8c8ba9cac26c51886dba245edad11aa14cec4e
Fabian Piper <fabianpiper@web.de>
2023-08-04 13:24:43 +0200

    chore: setup commitizen and commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+/** @type {import('@commitlint/types').UserConfig} */
+const CommitLintConfiguration = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // add your own scope here if needed
+  },
+}
+
+module.exports = CommitLintConfiguration
