dfd0d034ed5e51dd79ac0a81b700929058e148f9
Fabian Piper <fabianpiper@web.de>
2023-08-04 11:24:18 +0200

    feat: init husky with lint-staged

--- frontend/apps/web/.lintstagedrc.js
+++ frontend/apps/web/.lintstagedrc.js
@@ -1,4 +1,4 @@
-export default {
-  'src/**/*.{js,jsx,ts,tsx}': ['eslint --fix --max-warnings=0', 'prettier --write'],
+module.exports = {
+  'src/**/*.{js,jsx,ts,tsx}': ['eslint --fix --max-warnings=0 --no-error-on-unmatched-pattern'],
   'src/**/*.{json,css,scss,md}': ['prettier --write'],
 }
