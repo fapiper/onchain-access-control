c4ffbc0692fc0ca6637c249fc3b26a7addc18975
Fabian Piper <fabianpiper@web.de>
2023-08-04 11:09:00 +0200

    fix: helper image default props

--- frontend/apps/web/src/lib/components/samples/HelperImage.tsx
+++ frontend/apps/web/src/lib/components/samples/HelperImage.tsx
@@ -7,16 +7,10 @@ type HelperImageProps = {
 
 const size = 5;
 
-const HelperImage = ({ label, src }: HelperImageProps) => {
+export default function HelperImage({ label, src }: HelperImageProps) {
   return (
     <Tooltip hasArrow aria-label={label} label={label} placement="auto-end">
       <Image src={src} alt={label} title={label} height={size} width={size} />
     </Tooltip>
   );
 };
-
-HelperImage.defaultProps = {
-  label: '',
-};
-
-export default HelperImage;
