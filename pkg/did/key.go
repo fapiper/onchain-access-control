9be31920040f2cfd278a0c8b35e0872c107124fc
Fabian Piper <fabianpiper@web.de>
2023-08-28 11:56:51 +0200

    feat: seperated did package for import in entities

---
+++ pkg/did/key.go
@@ -0,0 +1,149 @@
+package did
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/TBD54566975/ssi-sdk/crypto"
+	"github.com/TBD54566975/ssi-sdk/did"
+	"github.com/TBD54566975/ssi-sdk/did/key"
+	"github.com/fapiper/onchain-access-control/pkg/common"
+	"github.com/fapiper/onchain-access-control/pkg/keystore"
+	"github.com/mr-tron/base58"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+)
+
+func NewKeyHandler(s *Storage, ks *keystore.Service) (MethodHandler, error) {
+	if s == nil {
+		return nil, errors.New("storage cannot be empty")
+	}
+	if ks == nil {
+		return nil, errors.New("keystore cannot be empty")
+	}
+	return &keyHandler{method: did.KeyMethod, storage: s, keyStore: ks}, nil
+}
+
+type keyHandler struct {
+	method   did.Method
+	storage  *Storage
+	keyStore *keystore.Service
+}
+
+var _ MethodHandler = (*keyHandler)(nil)
+
+func (h *keyHandler) GetMethod() did.Method {
+	return h.method
+}
+
+func (h *keyHandler) CreateDID(ctx context.Context, request CreateDIDRequest) (*CreateDIDResponse, error) {
+	logrus.Debugf("creating DID: %+v", request)
+
+	// create the DID
+	privKey, doc, err := key.GenerateDIDKey(request.KeyType)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not create did:key")
+	}
+
+	// expand it to the full docs for storage
+	expanded, err := doc.Expand()
+	if err != nil {
+		return nil, errors.Wrap(err, "error generating did:key document")
+	}
+
+	// store metadata in DID storage
+	id := doc.String()
+	storedDID := DefaultStoredDID{
+		ID:          id,
+		DID:         *expanded,
+		SoftDeleted: false,
+	}
+	if err = h.storage.StoreDID(ctx, storedDID); err != nil {
+		return nil, errors.Wrap(err, "could not store did:key value")
+	}
+
+	// convert to a serialized format for return to the client
+	privKeyBytes, err := crypto.PrivKeyToBytes(privKey)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not encode private key as base58")
+	}
+	privKeyBase58 := base58.Encode(privKeyBytes)
+
+	// store private key in key storage
+	keyStoreRequest := keystore.StoreKeyRequest{
+		ID:               expanded.VerificationMethod[0].ID,
+		Type:             request.KeyType,
+		Controller:       id,
+		PrivateKeyBase58: privKeyBase58,
+	}
+
+	if err = h.keyStore.StoreKey(ctx, keyStoreRequest); err != nil {
+		return nil, errors.Wrap(err, "could not store did:key private key")
+	}
+	return &CreateDIDResponse{DID: storedDID.DID}, nil
+}
+
+func (h *keyHandler) GetDID(ctx context.Context, request GetDIDRequest) (*GetDIDResponse, error) {
+	logrus.Debugf("getting DID: %+v", request)
+
+	id := request.ID
+	gotDID, err := h.storage.GetDIDDefault(ctx, id)
+	if err != nil {
+		return nil, fmt.Errorf("error getting DID: %s", id)
+	}
+	if gotDID == nil {
+		return nil, fmt.Errorf("did with id<%s> could not be found", id)
+	}
+	return &GetDIDResponse{DID: gotDID.DID}, nil
+}
+
+func (h *keyHandler) ListDIDs(ctx context.Context, page *common.Page) (*ListDIDsResponse, error) {
+	gotDIDs, err := h.storage.ListDIDsPage(ctx, did.KeyMethod.String(), page, new(DefaultStoredDID))
+	if err != nil {
+		return nil, errors.Wrap(err, "listing did:web DIDs page")
+	}
+	dids := make([]did.Document, 0, len(gotDIDs.DIDs))
+	for _, gotDID := range gotDIDs.DIDs {
+		if !gotDID.IsSoftDeleted() {
+			dids = append(dids, gotDID.GetDocument())
+		}
+	}
+	return &ListDIDsResponse{
+		DIDs:          dids,
+		NextPageToken: gotDIDs.NextPageToken,
+	}, nil
+}
+
+// ListDeletedDIDs returns only DIDs we have in storage for Key with SoftDeleted flag set to true
+func (h *keyHandler) ListDeletedDIDs(ctx context.Context) (*ListDIDsResponse, error) {
+	logrus.Debug("listing did:key DIDs")
+
+	gotDIDs, err := h.storage.ListDIDsDefault(ctx, did.KeyMethod.String())
+	if err != nil {
+		return nil, fmt.Errorf("error getting did:key DIDs")
+	}
+	dids := make([]did.Document, 0, len(gotDIDs))
+	for _, gotDID := range gotDIDs {
+		if gotDID.IsSoftDeleted() {
+			dids = append(dids, gotDID.GetDocument())
+		}
+	}
+	return &ListDIDsResponse{DIDs: dids}, nil
+}
+
+func (h *keyHandler) SoftDeleteDID(ctx context.Context, request DeleteDIDRequest) error {
+	logrus.Debugf("soft deleting DID: %+v", request)
+
+	id := request.ID
+	gotStoredDID, err := h.storage.GetDIDDefault(ctx, id)
+	if err != nil {
+		return fmt.Errorf("error getting DID: %s", id)
+	}
+	if gotStoredDID == nil {
+		return fmt.Errorf("did with id<%s> could not be found", id)
+	}
+
+	gotStoredDID.SoftDeleted = true
+
+	return h.storage.StoreDID(ctx, *gotStoredDID)
+}
---
+++ pkg/did/model.go
@@ -0,0 +1,110 @@
+package did
+
+import (
+	gocrypto "crypto"
+
+	"github.com/TBD54566975/ssi-sdk/crypto"
+	didsdk "github.com/TBD54566975/ssi-sdk/did"
+	"github.com/TBD54566975/ssi-sdk/did/ion"
+	"github.com/TBD54566975/ssi-sdk/did/resolution"
+	"github.com/fapiper/onchain-access-control/pkg/common"
+)
+
+type GetSupportedMethodsResponse struct {
+	Methods []didsdk.Method `json:"method"`
+}
+
+type ResolveDIDRequest struct {
+	DID string `json:"did" validate:"required"`
+}
+
+type ResolveDIDResponse struct {
+	ResolutionMetadata  *resolution.Metadata         `json:"didResolutionMetadata,omitempty"`
+	DIDDocument         *didsdk.Document             `json:"didDocument"`
+	DIDDocumentMetadata *resolution.DocumentMetadata `json:"didDocumentMetadata,omitempty"`
+}
+
+type CreateDIDRequestOptions interface {
+	Method() didsdk.Method
+}
+
+// CreateDIDRequest is the JSON-serializable request for creating a DID across DID method
+type CreateDIDRequest struct {
+	Method  didsdk.Method           `json:"method" validate:"required"`
+	KeyType crypto.KeyType          `validate:"required"`
+	Options CreateDIDRequestOptions `json:"options"`
+}
+
+// CreateDIDResponse is the JSON-serializable response for creating a DID
+type CreateDIDResponse struct {
+	DID didsdk.Document `json:"did"`
+}
+
+type BatchCreateDIDsRequest struct {
+	Requests []CreateDIDRequest `json:"requests"`
+}
+
+type BatchCreateDIDsResponse struct {
+	DIDs []didsdk.Document `json:"dids"`
+}
+
+type GetDIDRequest struct {
+	Method didsdk.Method `json:"method" validate:"required"`
+	ID     string        `json:"id" validate:"required"`
+}
+
+// GetDIDResponse is the JSON-serializable response for getting a DID
+type GetDIDResponse struct {
+	DID didsdk.Document `json:"did"`
+}
+
+type GetKeyFromDIDRequest struct {
+	ID    string `json:"id" validate:"required"`
+	KeyID string `json:"keyId,omitempty"`
+}
+
+type GetKeyFromDIDResponse struct {
+	KeyID     string             `json:"keyId"`
+	PublicKey gocrypto.PublicKey `json:"publicKey"`
+}
+
+type ListDIDsRequest struct {
+	Method  didsdk.Method `json:"method" validate:"required"`
+	Deleted bool          `json:"deleted"`
+
+	PageRequest *common.Page
+}
+
+// ListDIDsResponse is the JSON-serializable response for getting all DIDs for a given method
+type ListDIDsResponse struct {
+	DIDs          []didsdk.Document `json:"dids"`
+	NextPageToken string
+}
+
+type DeleteDIDRequest struct {
+	Method didsdk.Method `json:"method" validate:"required"`
+	ID     string        `json:"id" validate:"required"`
+}
+
+type UpdateIONDIDRequest struct {
+	DID ion.ION `json:"did"`
+
+	StateChange ion.StateChange `json:"stateChange"`
+}
+
+type UpdateIONDIDResponse struct {
+	DID didsdk.Document `json:"did"`
+}
+
+type UpdateRequestStatus string
+
+func (s UpdateRequestStatus) Bytes() []byte {
+	return []byte(s)
+}
+
+const (
+	PreAnchorStatus   UpdateRequestStatus = "pre-anchor"
+	AnchorErrorStatus UpdateRequestStatus = "anchor-error"
+	AnchoredStatus    UpdateRequestStatus = "anchored"
+	DoneStatus        UpdateRequestStatus = "done"
+)
