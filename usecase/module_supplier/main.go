9a25af592fabad1830c64e4aa544274723a3bb3c
Fabian Piper <fabianpiper@web.de>
2023-12-11 07:42:09 +0100

    refactor: rename usecase parties

--- usecase/module_supplier/main.go
+++ usecase/module_supplier/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"github.com/fapiper/onchain-access-control/pkg/issuer"
+	"github.com/fapiper/onchain-access-control/pkg/consumer"
 	"github.com/fapiper/onchain-access-control/pkg/owner"
 )
 
 func main() {
 	owner.Init()
-	issuer.Init()
+	consumer.Init()
 }
