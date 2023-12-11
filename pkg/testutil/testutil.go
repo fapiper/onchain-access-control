b54d4fc2d1eceaf8cad59006616e2fa01614c9d4
Fabian Piper <fabianpiper@web.de>
2023-12-11 11:20:16 +0100

    test: move auth service creation to common utils

--- pkg/testutil/testutil.go
+++ pkg/testutil/testutil.go
@@ -1,8 +1,14 @@
 package testutil
 
 import (
+	didsdk "github.com/TBD54566975/ssi-sdk/did"
 	miniredis "github.com/alicebob/miniredis/v2"
+	"github.com/fapiper/onchain-access-control/config"
+	"github.com/fapiper/onchain-access-control/pkg/service/auth"
+	"github.com/fapiper/onchain-access-control/pkg/service/did"
+	"github.com/fapiper/onchain-access-control/pkg/service/keystore"
 	"github.com/fapiper/onchain-access-control/pkg/storage"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
@@ -63,4 +69,46 @@ func setupRedisTestDB(t *testing.T) storage.ServiceStorage {
 	})
 
 	return s
+}
+
+func CreateTestAuthService(t *testing.T) (*auth.Service, error) {
+	file, err := os.CreateTemp("", "bolt")
+	require.NoError(t, err)
+	name := file.Name()
+	assert.NoError(t, file.Close())
+	s, err := storage.NewStorage(storage.Bolt, storage.Option{
+		ID:     storage.BoltDBFilePathOption,
+		Option: name,
+	})
+	assert.NoError(t, err)
+	assert.NotEmpty(t, s)
+
+	// remove the db file after the test
+	t.Cleanup(func() {
+		_ = s.Close()
+		_ = os.Remove(s.URI())
+	})
+
+	servicesConfig := new(config.ServicesConfig)
+	servicesConfig.DIDConfig.Methods = []string{didsdk.KeyMethod.String()}
+
+	keyEncrypter, keyDecrypter, err := keystore.NewServiceEncryption(s, servicesConfig.KeyStoreConfig.EncryptionConfig, keystore.ServiceKeyEncryptionKey)
+	require.NoError(t, err)
+	require.NotEmpty(t, keyEncrypter, keyDecrypter)
+
+	keyStoreServiceFactory := keystore.NewKeyStoreServiceFactory(servicesConfig.KeyStoreConfig, s, keyEncrypter, keyDecrypter)
+	require.NotEmpty(t, keyStoreServiceFactory)
+
+	keyStoreService, err := keyStoreServiceFactory(s)
+	require.NoError(t, err)
+	require.NotEmpty(t, keyStoreService)
+
+	didService, err := did.NewDIDService(servicesConfig.DIDConfig, s, keyStoreService, keyStoreServiceFactory)
+	require.NoError(t, err)
+	require.NotEmpty(t, didService)
+
+	didResolver := didService.GetResolver()
+	require.NotEmpty(t, didResolver)
+
+	return auth.NewAuthService(servicesConfig.AuthConfig, s, didResolver, keyStoreService)
 }
