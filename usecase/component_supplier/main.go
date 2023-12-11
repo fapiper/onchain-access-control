699ac0be3eee07a278379f726279837b40351c2a
Fabian Piper <fabianpiper@web.de>
2023-12-11 07:41:29 +0100

    feat: init ttp for cred issuance

--- usecase/component_supplier/main.go
+++ usecase/component_supplier/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"github.com/fapiper/onchain-access-control/pkg/issuer"
 	"github.com/fapiper/onchain-access-control/pkg/owner"
 )
 
 func main() {
 	owner.Init()
-	issuer.Init()
 }
