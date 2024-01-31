fd5fa7275891b71c98aa54d5710b2836026f9572
Fabian Piper <fabianpiper@web.de>
2024-01-31 14:20:13 +0100

    feat: init context handler contract

---
+++ pkg/contracts/src/AccessContextHandler.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "./context/IContextHandler.sol";
+import "./context/ContextHandlerBase.sol";
+import "./session/SessionRecipient.sol";
+
+contract AccessContextHandler is IContextHandler, SessionRecipient, ContextHandlerBase {
+ 
+    constructor() SessionRecipient(address(0)) {}
+ 
+    function createContextInstance(
+        bytes32 _id,
+        string memory _did
+    ) onlyContextAdmin(_id, _did) external {
+        // TODO deploy access context
+        IContextInstance _ctx = IContextInstance(0);
+        _setContextInstance(_id, _ctx);
+    }
+
+    function setSessionRegistry(
+        address sessionRegistry
+    ) external {
+        _setSessionRegistry(sessionRegistry);
+    }
+
+    function startSession(
+        bytes32 _roleContext,
+        bytes32 _role,
+        string memory _did,
+        bytes32[] memory _policyContexts,
+        bytes32[] memory _policies,
+        bytes[] memory _args,
+        bytes32 _tokenId,
+        bytes32 _token
+    ) external {
+        _getContextInstance(_roleContext).grantRole(_role, _did, _policyContexts, _policies, _args);
+        _getSessionRegistry().startSession(_tokenId, _token, _did);
+    }
+
+    function deleteContextInstance(
+        bytes32 _id,
+        string memory _did
+    ) onlyContextAdmin(_id, _did) external {
+        _setContextInstance(_id, address(0));
+    }
+
+    function getContextInstance(bytes32 _id) external view returns (IContextInstance) {
+        return _getContextInstance(_id);
+    }
+}
