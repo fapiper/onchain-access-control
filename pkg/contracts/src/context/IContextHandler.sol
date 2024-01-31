c3a3f68fc32b62c522ec0c8419786d3d66f45abc
Fabian Piper <fabianpiper@web.de>
2024-01-31 14:35:22 +0100

    fix: contexthandler interface add did for access

--- pkg/contracts/src/context/IContextHandler.sol
+++ pkg/contracts/src/context/IContextHandler.sol
@@ -5,7 +5,7 @@ import "./IContextInstance.sol";
 
 interface IContextHandler {
 
-    function createContextInstance(bytes32 _id) external;
-    function deleteContextInstance(bytes32 _id) external;
+    function createContextInstance(bytes32 _id, bytes32 _did) external;
+    function deleteContextInstance(bytes32 _id, bytes32 _did) external;
     function getContextInstance(bytes32 _id) external view returns (IContextInstance);
 }
