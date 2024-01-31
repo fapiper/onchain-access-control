6beb9339e99465d2fecc3e2e3d06be2295b0bc21
Fabian Piper <fabianpiper@web.de>
2024-01-31 14:20:54 +0100

    feat: use extensions in access context instance

--- pkg/contracts/src/AccessContext.sol
+++ pkg/contracts/src/AccessContext.sol
@@ -1,20 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.20;
 
 import "./did/DIDOwnable.sol";
-import "./ContextRecipient.sol";
-import "./Policies.sol";
-import "./Roles.sol";
-import "./AccessControlList.sol";
+import "./context/ContextInstance.sol";
+import "./context/IContextInstance.sol";
+import "./extension/AccessControlListExtension.sol";
+import "./extension/PolicyExtension.sol";
+import "./extension/RoleExtension.sol";
+import "./extension/PermissionExtension.sol";
 
-contract AccessContext is DIDOwnable, ContextRecipient, AccessControlList, Policies, Roles {
+contract AccessContext is IContextInstance, DIDOwnable, AccessControlListExtension, PermissionExtension, PolicyExtension, RoleExtension {
 
     constructor(
         address initialOwner,
         address contextId,
         address contextHandler,
         address didRegistry
-    ) DIDOwnable(initialOwner, didRegistry) ContextRecipient(contextId, contextHandler) {}
+    ) DIDOwnable(initialOwner, didRegistry) PolicyExtension(contextId, contextHandler) {}
 
     /**
      *  @notice         Allows only context admin (owner) or role member for `_role`.
@@ -33,8 +35,9 @@ contract AccessContext is DIDOwnable, ContextRecipient, AccessControlList, Polic
      *
      *  @param _role           Uid of the role within this context.
      *  @param _did            DID of the user.
-     *  @param _contract       Address of the policy contract.
-     *  @param _verify         Function identifier of the policy verification function of `_contract`.
+     *  @param _policyContexts Uid of policy contexts.
+     *  @param _policies       Uids of the policies of context in `policyContexts` at same index.
+     *  @param _args           Verification function arguments.
      */
     function grantRole(
         bytes32 _role,
@@ -45,10 +48,24 @@ contract AccessContext is DIDOwnable, ContextRecipient, AccessControlList, Polic
     ) external {
         bytes32 thisContext = _thisContext();
         require(_hasRoleExpectedPolicyCount(thisContext, _role, _policies.length), "policy len not allowed");
-        Policy[] memory policies = _getPolicies(_policyContexts, _policies);
-        require(_hasRolePolicies(thisContext, _role, policies), "policy for role not allowed");
+        // TODO with get policies or only policy context sufficient?
+        require(_hasRolePolicies(thisContext, _role, _getPolicies(_policyContexts, _policies)), "policy for role not allowed");
+        // TODO use policy struct instead of bytes. Or even better (possible?): use policy context and bytes32 id sufficient?
         require(_verifyPolicies(_policyContexts, _policies, _args), "not allowed");
         _grantRole(_role, _did);
+    }
+
+    /**
+     *  @notice         Checks if a given did is admin.
+     *
+     *  @param _did         DID of the admin of this context.
+     *  @param _account     Address of the DID controller.
+     */
+    function checkAdmin(
+        string memory _did,
+        address _account
+    ) external view returns (bool) {
+       return _checkOwner(_did, _account);
     }
 
     /**
@@ -57,39 +74,13 @@ contract AccessContext is DIDOwnable, ContextRecipient, AccessControlList, Polic
      *                  Emits {RoleRevoke Event}.
      *
      *  @param _role           Uid of the role within this context.
-     *  @param _policy         Uid of the policy within this context.
-     *  @param _contract       Address of the policy contract.
-     *  @param _verify         Function identifier of the policy verification function of `_contract`.
+     *  @param _did            DID of the admin of this context.
      */
     function revokeRole(
         bytes32 _role,
         string memory _did
     ) external onlyOwnerOrRole(_role) {
         _revokeRole(_role, _did);
-    }
-
-    /**
-     *  @notice         Sets up a role by registering a policy and assigning it to a role, registering
-     *                  permissions for a resource and assigning them to a role.
-     *  @dev            Caller must have owner role.
-     *                  Emits {RoleSetup Event}.
-     *
-     *  @param _role           Uid of the role within this context.
-     *  @param _policy         Uid of the policy within this context.
-     *  @param _instance       Address of the policy contract instance.
-     *  @param _verify         Function identifier of the policy verification function of `_contract`.
-     */
-    function setupRole(
-        bytes32 _role,
-        bytes32 _policy,
-        address _instance,
-        bytes4 _verify
-    ) external onlyOwner {
-        bytes32 thisContext = _thisContext();
-        _registerPolicy(thisContext, _policy, _instance, _verify);
-        _assignPolicyToRole(thisContext, _role, thisContext, _policies);
-        _registerPermissionForResource(_permission, _resource, _operations);
-        _assignPermissionToRole(thisContext, _role, thisContext, _permission);
     }
 
     /**
@@ -112,11 +103,41 @@ contract AccessContext is DIDOwnable, ContextRecipient, AccessControlList, Polic
         bytes32 _permission,
         bytes32 _resource,
         Operation[] memory _operations
-    ) external overwrite onlyOwner {
+    ) external onlyOwner {
         bytes32 thisContext = _thisContext();
         _assignPolicyToRole(thisContext, _role, _policyContext, _policy);
         _registerPermissionForResource(_permission, _resource, _operations);
         _assignPermissionToRole(thisContext, _role, _permission);
+    }
+
+    /**
+     *  @notice         Sets up a role by registering a policy and assigning it to a role, registering
+     *                  permissions for a resource and assigning them to a role.
+     *  @dev            Caller must have owner role.
+     *                  Emits {RoleSetup Event}.
+     *
+     *  @param _role           Uid of the role within this context.
+     *  @param _policy         Uid of the policy within this context.
+     *  @param _permission     Uid of the permission.
+     *  @param _resource       Uid of the resource for which to assign permissions to.
+     *  @param _operations     Permitted operations for the resource. Currently either [READ] or [READ, WRITE].
+     *  @param _instance       Address of the policy contract instance.
+     *  @param _verify         Function identifier of the policy verification function of `_contract`.
+     */
+    function setupRole(
+        bytes32 _role,
+        bytes32 _policy,
+        bytes32 _permission,
+        bytes32 _resource,
+        Operation[] memory _operations,
+        address _instance,
+        bytes4 _verify
+    ) external onlyOwner {
+        bytes32 thisContext = _thisContext();
+        _registerPolicy(thisContext, _policy, _instance, _verify);
+        _assignPolicyToRole(thisContext, _role, thisContext, _policy);
+        _registerPermissionForResource(_permission, _resource, _operations);
+        _assignPermissionToRole(thisContext, _role, thisContext, _permission);
     }
 
     /**
@@ -151,10 +172,40 @@ contract AccessContext is DIDOwnable, ContextRecipient, AccessControlList, Polic
         address _contract,
         bytes4 _verify,
         bytes32 _role
-    ) external overwrite onlyOwner {
+    ) external onlyOwner {
         bytes32 thisContext = _thisContext();
         _registerPolicy(_policy, _contract, _verify);
         _assignPolicyToRole(thisContext, _role, thisContext, _policy);
+    }
+
+    /**
+     *  @notice         Get a policy from own or cross context.
+     *
+     *  @param _context        Uid of the policy context.
+     *  @param _id             Uid of the role within `_context`.
+     *
+     * @return policy          The policy struct of `_context`
+     */
+    function getPolicy(
+        bytes32 _context,
+        bytes32 _id
+    ) public returns (Policy memory) {
+        return _getPolicy(_context, _id);
+    }
+
+    /**
+     *  @notice         Get policies from own or cross context.
+     *
+     *  @param _contexts        Uids of the policy contexts.
+     *  @param _ids             Uids of the roles within `_contexts`.
+     *
+     * @return policies          The policy struct array of `_contexts`
+     */
+    function getPolicies(
+        bytes32[] memory _contexts,
+        bytes32[] memory _ids
+    ) external returns (Policy[] memory policies) {
+        return _getPolicies(_contexts, _ids);
     }
 
     /**
@@ -208,7 +259,7 @@ contract AccessContext is DIDOwnable, ContextRecipient, AccessControlList, Polic
         Operation[] memory _operations,
         bytes32 _roleContext,
         bytes32 _role
-    ) external overwrite onlyOwner {
+    ) external onlyOwner {
         _registerPermissionForResource(_permission, _resource, _operations);
         _assignPermissionToRole(_roleContext, _role, _permission);
     }
@@ -245,44 +296,5 @@ contract AccessContext is DIDOwnable, ContextRecipient, AccessControlList, Polic
         bytes32 _role
     ) external onlyOwner {
         _unassignPermissionToRole(_roleContext, _role, _permission);
-    }
-
-    /**
-     *  @notice         Get a policy from own or cross context.
-     *
-     *  @param _context        Uid of the policy context.
-     *  @param _id             Uid of the role within `_context`.
-     *
-     * @return policy          The policy struct of `_context`
-     */
-    function getPolicy(
-        bytes32 _context,
-        bytes32 _id
-    ) public returns (Policy memory policy) {
-        if(_context == _thisContext()){
-            return _getPolicy(_thisContext(), _policy);
-        } else {
-            return _context(_context).getPolicy(_policy);
-        }
-    }
-
-    /**
-     *  @notice         Get policies from own or cross context.
-     *
-     *  @param _contexts        Uids of the policy contexts.
-     *  @param _ids             Uids of the roles within `_contexts`.
-     *
-     * @return policies          The policy struct array of `_contexts`
-     */
-    function _getPolicies(
-        bytes32[] memory _contexts,
-        bytes32[] memory _ids
-    ) internal returns (Policy[] memory policies) {
-        uint256 len = _contexts.length;
-        policies = new Policy[](len);
-        for (uint256 i = 0; i < len; i++) {
-            policies[i] = getPolicy(_contexts[i], _ids[i]);
-        }
-        return policies;
     }
 }
