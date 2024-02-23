03875f1a0767055ee7c6feede8d4d2686b2b8417
Fabian Piper <fabianpiper@web.de>
2024-02-23 10:01:33 +0100

    refactor: framework setup using engine

--- core/server/framework/server.go
+++ core/server/framework/server.go
@@ -2,15 +2,12 @@ package framework
 
 import (
 	"context"
-	"net/http"
-	"os"
-
+	"github.com/fapiper/onchain-access-control/core/config"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
-	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/trace"
-
-	"github.com/fapiper/onchain-access-control/core/config"
+	"net/http"
+	"os"
 )
 
 const (
@@ -43,24 +40,18 @@ func (s *Server) PreShutdownHooks(ctx context.Context) error {
 	return nil
 }
 
-// NewServer creates a Server that handles a set of routes for the application.
-func NewServer(cfg config.ServerConfig, handler *gin.Engine, shutdown chan os.Signal) *Server {
-	var tracer trace.Tracer
-	if cfg.JagerEnabled {
-		tracer = otel.Tracer(serviceName)
+func SetupEngine(ctx context.Context, c config.ServerConfig) *gin.Engine {
+	engine := gin.New()
+	gin.ForceConsoleColor()
+
+	switch c.Environment {
+	case config.EnvironmentDev:
+		gin.SetMode(gin.DebugMode)
+	case config.EnvironmentTest:
+		gin.SetMode(gin.TestMode)
+	case config.EnvironmentProd:
+		gin.SetMode(gin.ReleaseMode)
 	}
 
-	return &Server{
-		Server: &http.Server{
-			Addr:              cfg.APIHost,
-			Handler:           handler,
-			ReadTimeout:       cfg.ReadTimeout,
-			ReadHeaderTimeout: cfg.ReadTimeout,
-			WriteTimeout:      cfg.WriteTimeout,
-		},
-		router:      handler,
-		tracer:      tracer,
-		shutdown:    shutdown,
-		preShutdown: make([]func(ctx context.Context) error, 0),
-	}
+	return engine
 }
