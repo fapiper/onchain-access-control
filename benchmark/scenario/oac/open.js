1b38e788fdef6731f5195d404c479b3e36b761d7
Fabian Piper <fabianpiper@web.de>
2024-02-25 12:21:51 +0100

    feat: init custom benchmark setup

---
+++ benchmark/scenario/oac/open.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const OperationBase = require('./utils/operation-base')
+const SimpleState = require('./utils/simple-state')
+
+/**
+ * Workload module for initializing the SUT with various accounts.
+ */
+class Open extends OperationBase {
+  /**
+   * Initializes the parameters of the workload.
+   */
+  constructor() {
+    super()
+  }
+
+  /**
+   * Create an empty state representation.
+   * @return {SimpleState} The state instance.
+   */
+  createSimpleState() {
+    return new SimpleState(this.workerIndex, this.initialMoney, this.moneyToTransfer)
+  }
+
+  /**
+   * Assemble TXs for opening new accounts.
+   */
+  async submitTransaction() {
+    let createArgs = this.simpleState.getOpenAccountArguments()
+    await this.sutAdapter.sendRequests(this.createConnectorRequest('open', createArgs))
+  }
+}
+
+/**
+ * Create a new instance of the workload module.
+ * @return {WorkloadModuleInterface}
+ */
+function createWorkloadModule() {
+  return new Open()
+}
+
+module.exports.createWorkloadModule = createWorkloadModule
