4e1c66cc1c59a42ddb78e370bba1c7806bc86a31
Fabian Piper <fabianpiper@web.de>
2024-02-29 06:38:39 +0100

    refactor: deploy policy task callback

--- contracts/scripts/tasks/deployPolicy.ts
+++ contracts/scripts/tasks/deployPolicy.ts
@@ -2,7 +2,6 @@ import { task } from "hardhat/config";
 
 import { simpleDeploy } from "../deploy";
 
-task("deploy-policy", "Deploy a sample policy", async (taskArgs: { name?: string }, hre) => {
-  console.log("deploying policy...", taskArgs);
-  return simpleDeploy(taskArgs.name ?? "Verifier")(hre);
-});
+task("deploy-policy", "Deploy a sample policy")
+  .addOptionalParam("policyName", "The policy's name", "Verifier")
+  .setAction(async (taskArgs: { policyName: string }, hre) => simpleDeploy(taskArgs.policyName)(hre));
