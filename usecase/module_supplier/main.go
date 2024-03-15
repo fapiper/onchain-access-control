fb1300c6bf2b5a5cd1453301bc20b696f8c2a06f
Fabian Piper <fabianpiper@web.de>
2024-03-15 21:04:34 +0100

    refactor: cleanup resourceowner deps

--- usecase/module_supplier/main.go
+++ usecase/module_supplier/main.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"github.com/fapiper/onchain-access-control/core/consumer"
-	"github.com/fapiper/onchain-access-control/core/owner"
-)
-
 func main() {
-	owner.Init()
-	consumer.Init()
+	resourceowner.Init()
 }
